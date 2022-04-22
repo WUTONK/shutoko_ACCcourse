5bcf282ef8a92a6b982dab478b49001a94d08b00
WUZITONG <wjt474696120@gmail.com>
2022-04-22 17:32:34 +0800

    update imgs

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -6,12 +6,37 @@ module.exports = {
     title: '首都高教程',
     description: '教程文档',
 
+    //定义绝对路径
+    chainWebpack: config => {
+      config.resolve.alias
+        .set('_zhcn_img_', resolve('/zh_cn/imgs')) //中文图片路径
+        .set('_usen_img_',resolve('/us_en/imgs')) //英文图片路径
+        .set('_zh_cn_',resolve('/zh_cn')) //中文页面路径
+        .set('_us_en_',resolve('/us_en')) //英文页面路径
+        .set('',resolve(''))
+        .set('',resolve(''))
+    },
+
     themeConfig: {
       sidebar: [
-       '/home1'
-      ],
-      sidebar: 'auto',
-      sidebarDepth: 3,
+        {
+          title: '主页',
+          collapsable: false,
+          children: [
+            '/', '/page-localGame/install',
+          ]
+        },
+        {
+          title: '二组题目',
+          children: [
+            ['/home2', 'home2自定义题目'], 
+            '/home3',
+            '/中文',
+          ]
+        },
+      ],  
+      sidebar: 'auto', //自动显示奇遇侧边栏
+      sidebarDepth: 3, //显示到三级标题
       nav: [
         { text: 'Home', link: '/' },                      // 根路径
         { text: 'Guide', link: '/guide/' },
@@ -22,8 +47,6 @@ module.exports = {
           items: [
             { text: 'Chinese', link: '/language/chinese' },
             { text: 'English', link: '/language/English' },
-            { text: 'Chinese', link: '/language/chinese' },
-            { text: 'Chinese', link: '/language/chinese' },
             { text: 'Japanese', link: '/language/japanese' }
           ]
         },
@@ -40,8 +63,8 @@ module.exports = {
             { 
               text: '社交媒体', 
               items: [
-                { text: '推特', link: '/language/chinese' },
-                { text: '电报', link: '/language/chinese'},
+                { text: '推特', link: '/twitter/WUTONK' },
+                { text: '电报', link: '/WUTONK'},
               ] 
             },
           ]
